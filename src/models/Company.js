2cd6374ffe392742c31d78ebfaf7c064028ae225
Itziar <itzimr97@pm.me>
2026-04-09 10:13:37 +0200

    Add basic Company model schema

---
+++ src/models/Company.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const companySchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
+
+export default mongoose.model('Company', companySchema);
