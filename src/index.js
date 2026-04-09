2116f71398e1015174d42d94461573d6cfc44a81
Itziar <itzimr97@pm.me>
2026-04-09 13:05:53 +0200

    Add MongoDB connection before listening (fixed)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import app from './app.js'
+import { connectDB } from './config/index.js';
 
-// Default 3000 if port not specified
+// Default 3000 if port not specified.
 const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB before listening.
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
