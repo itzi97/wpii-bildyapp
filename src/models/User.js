dbbf3d4b87ab65de56d379a8ffc41535856cc08f
Itziar <itzimr97@pm.me>
2026-04-09 10:00:54 +0200

    Add basic User model schema

---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: { type: String, required: true, select: false }
+}, { timestamps: true });
+
+export default mongoose.model('User', userSchema);
