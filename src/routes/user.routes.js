b248ccca2b91e8f0258a5aec03efbaacffc1c200
Itziar <itzimr97@pm.me>
2026-04-09 12:43:36 +0200

    Add route skeleton

---
+++ src/routes/user.routes.js
@@ -0,0 +1,65 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// TODO: Implement.
+router.post('/register', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.put('/validation', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.post('/login', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.put('/register/profile', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.patch('/company', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.patch('/logo', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.get('/', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.post('/refresh', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.post('/logout', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.delete('/', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.put('/password', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+// TODO: Implement.
+router.post('/invite', (req, res) => {
+  res.status(501).json({ message: 'Not implemented yet' });
+});
+
+export default router;
