c6638031441f07df408fc0d795e69f53b9876f26
Itziar <itzimr97@pm.me>
2026-04-09 11:24:07 +0200

    Add register validation schema

---
+++ src/validators/user.validator.js
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+// Register POST /api/user/register
+export const registerSchema = z.object({
+  email: z
+    .string({ required_error: 'Email is required' })
+    .email('Must be a valid email')
+    .transform((val) => val.toLowerCase().trim()),
+  password: z
+    .string({ required_error: 'Password is required' })
+    .min(8, 'Password must be at least 8 characters')
+});
