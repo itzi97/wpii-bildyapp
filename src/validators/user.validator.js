40704589e468b293db7d7244ee9bca894e4aab63
Itziar <itzimr97@pm.me>
2026-04-09 11:25:38 +0200

    Add login validation schema

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -10,3 +10,13 @@ export const registerSchema = z.object({
     .string({ required_error: 'Password is required' })
     .min(8, 'Password must be at least 8 characters')
 });
+
+// Login POST /api/user/login
+export const loginSchema = z.object({
+  email: z
+    .string({ required_error: 'Email is required' })
+    .email('Must be a valid email')
+    .transform((val) => val.toLowerCase().trim()),
+  password: z
+    .string({ required_error: 'Password is required' })
+})
