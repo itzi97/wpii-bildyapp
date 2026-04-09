3b5b7af2852b6f4b13ef2290ebbbd4133145de26
Itziar <itzimr97@pm.me>
2026-04-09 15:34:05 +0200

    Add security middleware stack

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import mongoSanitize from 'express-mongo-sanitize';
+import cors from 'cors';
+import morgan from 'morgan';
+
 import errorHandler from './middleware/error-handler.js';
 import userRoutes from './routes/user.routes.js'
 
 const app = express();
 
+// Security middleware
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(rateLimit({ windowsMS: 15 * 60 * 1000, max: 100 }));
+app.use(mongoSanitize());
 app.use(express.json());
 
 app.get('/', (req, res) => {
