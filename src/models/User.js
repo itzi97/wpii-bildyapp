1e9817c1fc3c7f0388451a65806b471d574ef9eb
Itziar <itzimr97@pm.me>
2026-04-09 10:26:26 +0200

    Fixed user model indentation to make it more consistent

--- src/models/User.js
+++ src/models/User.js
@@ -9,7 +9,11 @@ const userSchema = new mongoose.Schema({
     trim: true,
     index: true // Redundant, keep explicit for clarity.
   },
-  password: { type: String, required: true, select: false },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
   role: {
     type: String,
     enum: ['admin', 'guest'],
@@ -22,11 +26,26 @@ const userSchema = new mongoose.Schema({
     default: 'pending',
     index: true
   },
-  verificationCode: { type: String },
-  verificationAttempts: { type: Number, default: 3 },
-  name: { type: String, trim: true },
-  lastName: { type: String, trim: true },
-  deleted: { type: Boolean, default: false, index: true }
+  verificationCode: {
+    type: String
+  },
+  verificationAttempts: {
+    type: Number,
+    default: 3
+  },
+  name: {
+    type: String,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    trim: true
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
+    index: true
+  }
 }, {
   timestamps: true,
   toJSON: { virtuals: true },
