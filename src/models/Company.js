0a0646400d4b68a84c52a1659b763005149fa6f0
Itziar <itzimr97@pm.me>
2026-04-09 10:15:25 +0200

    Add company name field

--- src/models/Company.js
+++ src/models/Company.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose';
 
 const companySchema = new mongoose.Schema({
+  // References User (schema declared in User.js).
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
   }
 }, { timestamps: true });
 
