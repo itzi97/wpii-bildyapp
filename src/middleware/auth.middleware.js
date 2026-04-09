3c1dcc2ac53580b93d55fa63a5168d980437dc01
Itziar <itzimr97@pm.me>
2026-04-09 11:52:19 +0200

    Add basic JWT token extraction middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,12 @@
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access token required' });
+  }
+
+  // TODO: verify token and attach user to req
+  req.user = { id: 'temp' };
+  next();
+};
