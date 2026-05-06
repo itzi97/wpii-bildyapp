68ef0aa9d1ce95c816dcbda1967fa3d83db121cc
Itziar <itzimr97@pm.me>
2026-05-06 15:32:19 +0200

    feat(index): added graceful shutdown to index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 // src/index.js
+// src/index.js
 import server from './app.js'
 import { connectDB } from './config/index.js';
+import mongoose from 'mongoose';
+import { io } from './app.js';
 
-// Default 3000 if port not specified.
 const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB before listening.
 await connectDB();
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
   console.log(`Socket.IO server active`);
 });
+
+// Graceful shutdown
+const shutdown = async (signal) => {
+  console.log(`\n${signal} received. Shutting down gracefully...`);
+  server.close(async () => {
+    await mongoose.connection.close();
+    io.close();
+    console.log('Server, DB and Socket.IO closed.');
+    process.exit(0);
+  });
+};
+
+process.on('SIGTERM', () => shutdown('SIGTERM'));
+process.on('SIGINT', () => shutdown('SIGINT'));
